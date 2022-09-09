6399b5f81e5a161475cf1d881ceab5cdf91ab47d
Ben Balzer <b.g.balzer@gmail.com>
2022-09-09 18:07:23 -0400

    implemented resnet object detection pretrained model

--- loop_over_dataset.py
+++ loop_over_dataset.py
@@ -22,6 +22,7 @@ import math
 import cv2
 import matplotlib.pyplot as plt
 import copy
+import open3d as o3d
 
 ## Add current working directory to path
 sys.path.append(os.getcwd())
@@ -49,23 +50,23 @@ import misc.params as params
 ## Set parameters and perform initializations
 
 ## Select Waymo Open Dataset file and frame numbers
-# data_filename = 'training_segment-1005081002024129653_5313_150_5333_150_with_camera_labels.tfrecord' # Sequence 1
+data_filename = 'training_segment-1005081002024129653_5313_150_5333_150_with_camera_labels.tfrecord'  # Sequence 1
 # data_filename = 'training_segment-10072231702153043603_5725_000_5745_000_with_camera_labels.tfrecord' # Sequence 2
-data_filename = 'training_segment-10963653239323173269_1924_000_1944_000_with_camera_labels.tfrecord'  # Sequence 3
-show_only_frames = [0, 200]  # show only frames in interval for debugging
+# data_filename = 'training_segment-10963653239323173269_1924_000_1944_000_with_camera_labels.tfrecord' # Sequence 3
+show_only_frames = [50, 51]  # show only frames in interval for debugging
 
 ## Prepare Waymo Open Dataset file for loading
 data_fullpath = os.path.join(os.path.dirname(os.path.realpath(__file__)), 'dataset',
                              data_filename)  # adjustable path in case this script is called from another working directory
-model = "darknet"
-sequence = "3"
+model = "resnet"
+sequence = "1"
 results_fullpath = os.path.join(os.path.dirname(os.path.realpath(__file__)),
-                                'results/' + model + '/results_sequence_' + sequence + '_' + model)
+                                'results/' + 'fpn-' + model + '/results_sequence_' + sequence + '_' + model)  # added 'fpn-' after 'results/' due to workspace dir names being different
 datafile = WaymoDataFileReader(data_fullpath)
 datafile_iter = iter(datafile)  # initialize dataset iterator
 
 ## Initialize object detection
-configs_det = det.load_configs(model_name='darknet')  # options are 'darknet', 'fpn_resnet'
+configs_det = det.load_configs(model_name='fpn_resnet')  # options are 'darknet', 'fpn_resnet'
 model_det = det.create_model(configs_det)
 
 configs_det.use_labels_as_objects = False  # True = use groundtruth labels as objects, False = use model-based detection
@@ -82,10 +83,12 @@ camera = None  # init camera sensor object
 np.random.seed(10)  # make random values predictable
 
 ## Selective execution and visualization
-exec_detection = []  # options are 'bev_from_pcl', 'detect_objects', 'validate_object_labels', 'measure_detection_performance'; options not in the list will be loaded from file
+# exec_data = ['pcl_from_rangeimage'] #options are 'pcl_from_rangeimage'
+exec_detection = ['bev_from_pcl',
+                  'detect_objects']  # options are 'bev_from_pcl', 'detect_objects', 'validate_object_labels', 'measure_detection_performance'; options not in the list will be loaded from file
 exec_tracking = []  # options are 'perform_tracking'
 exec_visualization = [
-    'show_pcl']  # options are 'show_range_image', 'show_bev', 'show_pcl', 'show_labels_in_image', 'show_objects_and_labels_in_bev', 'show_objects_in_bev_labels_in_camera', 'show_tracks', 'show_detection_performance', 'make_tracking_movie'
+    'show_objects_in_bev_labels_in_camera']  # options are 'show_range_image', 'show_bev', 'show_pcl', 'show_labels_in_image', 'show_objects_and_labels_in_bev', 'show_objects_in_bev_labels_in_camera', 'show_tracks', 'show_detection_performance', 'make_tracking_movie'
 exec_list = make_exec_list(exec_detection, exec_tracking, exec_visualization)
 vis_pause_time = 0  # set pause time between frames in ms (0 = stop between frames until key is pressed)
 
@@ -184,7 +187,7 @@ while True:
             cv2.waitKey(vis_pause_time)
 
         if 'show_pcl' in exec_list:
-            pcl.show_pcl(lidar_pcl  )
+            pcl.show_pcl(lidar_pcl)
 
         if 'show_bev' in exec_list:
             tools.show_bev(lidar_bev, configs_det)
